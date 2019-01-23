8db7058d18025b158993c029c39052e2ecdd4ba3
khanhdang <khanhdangquocag97@gmail.com>
2019-01-23 16:25:35 +0700

    set rule for lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,33 @@
+import re
+import sys
+
+rules = [
+    (r"\d+", "NUMBER"),
+    (r"\;", "SEMI"),
+    (r"\:", "COLON"),
+    (r"\,", "COMMA"),
+    (r"\.", "DOT"),
+    (r"\(", "LPAREN"),
+    (r"\)", "RPAREN"),
+    (r"\<", "LT"),
+    (r"\>", "GT"),
+    (r"\=", "EQ"),
+    (r"\+", "PLUS"),
+    (r"\-", "MINUS"),
+    (r"\*", "MULTIPLY"),
+    (r"\/", "DIVIDE"),
+]
+
 path = "input.txt"
 input = ""
-with open('input.txt', 'r') as f:
-    input = f.readline()
-    
+with open("input.txt", "r") as f:
+    input = f.read().split()
+
+print(input)
+txt = "a<b"
+x = re.search(rules[7], txt)
+if x:
+    print("YES! We have a match!")
+else:
+    print("No match")
+
